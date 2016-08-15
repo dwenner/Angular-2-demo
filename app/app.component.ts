4f6a562a7e5835abb697228003ce8ddf4b76b5b5
Daniel Wenner <github@danielwenner.com>
2016-08-15 20:30:45 +0200

    Output til seperat 'dist' katalog. Bygg is VS code

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1><p>{{hello}}</p>'
+    template: '<h1>My First Angular 2 App</h1>'
 })
-export class AppComponent { 
-    hello: string = "Hello world";
-}
+export class AppComponent { }
--- systemjs.config.js
+++ systemjs.config.js
@@ -6,7 +6,7 @@
 
   // map tells the System loader where to look for things
   var map = {
-    'app':                        'app', // 'dist',
+    'app':                        'dist', // was 'app',
 
     '@angular':                   'node_modules/@angular',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
@@ -15,7 +15,7 @@
 
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
-    'app':                        { main: 'main.js',  defaultExtension: 'js' },
+    'app':                        { main: 'app/main.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
     'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
   };
