e9e8416c2730351b280c89b0501047fe00eee53f
Daniel Wenner <github@danielwenner.com>
2016-08-17 00:23:07 +0200

    ngFor

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/app.component.html'
+    template: `<h1>{{title}}</h1>
+                <p [style.color]="danielsFarge">Hello {{name}}</p>
+               `
 })
 export class AppComponent {
     // [ ] er property binding - Component til DOM
@@ -13,16 +15,25 @@ export class AppComponent {
     title = "Customer App";
     name = "Daniel";
 
+    customers = [
+        { id: 1, name: "Daniel" },
+        { id: 2, name: "Lars" },
+        { id: 3, name: "Hillary" },
+        { id: 4, name: "Donald" },
+        { id: 5, name: "Bill" }
+    ];
+
     changeSuitColor() {
         let defaultColor = "blue";
         this.danielsFarge = this.danielsFarge === defaultColor ? 'red' : defaultColor
     }
 
     //Strong type event. 
-    changeMyName(event: KeyboardEvent  ) {
+    changeMyName(event: KeyboardEvent) {
         //https://angular.io/docs/ts/latest/guide/user-input.html#keyup1
         this.name = (<HTMLInputElement>event.target).value;
     }
-    
-
+    changeMyNameWithValue(value: string) {
+        this.name = value;
+    }
 }
