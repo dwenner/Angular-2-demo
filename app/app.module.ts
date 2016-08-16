e3c6fa42bf0cf8bbcace9f3105bb4d316b37c841
Daniel Wenner <github@danielwenner.com>
2016-08-16 23:05:47 +0200

    NgModel 2 way binding.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms'; //Trengs for ngModel
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ BrowserModule, FormsModule ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
