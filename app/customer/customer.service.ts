908c6cf74f74290949766714bd32e75efa720d16
Daniel Wenner <github@danielwenner.com>
2016-08-17 21:44:19 +0200

    Feilhåndtering. Customer.service.ts og customers.component.ts

--- app/customer/customer.service.ts
+++ app/customer/customer.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs/Rx';
 
 const URL_CUSTOMER = 'app/customers.json';
 
@@ -8,12 +8,18 @@ const URL_CUSTOMER = 'app/customers.json';
 export class CustomerService {
 
 
-    constructor(private _http:  Http) { }
-    
+    constructor(private _http: Http) { }
+
     getCustomers() {
+        //Merk at .map på Obervavle er en del av RxJS
         let response = this._http.get(URL_CUSTOMER).map(
-            (response:  Response) => response.json()
-        );
+            (response: Response) => response.json()
+        ).catch(this._handleError);
         return response;
+    }
+    
+    private _handleError(err: any) {
+        console.log("from service: ", err); //gjør noe bedre her. Logg/fiks/osv...
+        return Observable.throw(err);
     }
 }
--- app/customer/customers.component.ts
+++ app/customer/customers.component.ts
@@ -11,17 +11,24 @@ import { Observable } from 'rxjs/Rx';
                 <app-customer [customer]="c" [my-color]="color"></app-customer>
                 </li>
             </ul>
+            <p *ngIf="unhandledError" style="color: red" >Noe er galt! :(</p>
             `
 })
 export class CustomersComponent implements OnInit {
     @Input("outer-color") color: string;
 
     customers: Observable<any[]>;
+    unhandledError = false;
 
     constructor(private _customerService: CustomerService) { }
 
     ngOnInit() {
-        this.customers = this._customerService.getCustomers();
+        this.customers = this._customerService.getCustomers()
+        .catch((err) => {
+            console.error("From component: ", err);
+            this.unhandledError = true;
+            var response = Observable.of([true]);
+            return response;
+        });
     }
-
 }
